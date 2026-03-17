9f92070d1d30b966029ca669ea107e7ea6d79e51
semkaakr <christinasemenyuk888@gmail.com>
2026-03-17 23:49:42 +0300

    3. Создан графический интерфейс с элементами управравления ActivityForm.cs

---
+++ HealthManager/HealthManager/ActivityForm.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HealthManager
+{
+    public partial class ActivityForm : Form
+    {
+        private TextBox activityTypeTextBox;
+        private TextBox durationTextBox;
+        private Label activityTypeLabel;
+        private Label durationLabel;
+        public string ActivityType { get; private set; }
+        public decimal Duration { get; private set; }
+        public ActivityForm()
+        {
+            this.Text = "Добавить активность";
+            this.Width = 300;
+            this.Height = 200;
+            CreateControls();
+        }
+        private void CreateControls()
+        {
+            activityTypeLabel = new Label
+            {
+                Text = "Тип активности:",
+                Location = new System.Drawing.Point(10, 10)
+            };
+            activityTypeTextBox = new TextBox
+            {
+                Location = new System.Drawing.Point(10, 30),
+                Size = new System.Drawing.Size(200, 20)
+            };
+            durationLabel = new Label
+            {
+                Text = "Продолжительность (минут):",
+                Location = new System.Drawing.Point(10, 55)
+            };
+            durationTextBox = new TextBox
+            {
+                Location = new System.Drawing.Point(10, 75),
+                Size = new System.Drawing.Size(200, 20)
+            };
+            var okButton = new Button
+            {
+                Text = "OK",
+                Location = new System.Drawing.Point(10, 100),
+                Size = new System.Drawing.Size(80, 25)
+            };
+            okButton.Click += (sender, e) =>
+            {
+                if (decimal.TryParse(durationTextBox.Text, out decimal duration))
+                {
+                    ActivityType = activityTypeTextBox.Text;
+                    Duration = duration;
+                    DialogResult = DialogResult.OK;
+                    Close();
+                }
+                else
+                {
+                    MessageBox.Show("Пожалуйста, введите корректное значение продолжительности.");
+                }
+            };
+            var cancelButton = new Button
+            {
+                Text = "Отмена",
+                Location = new System.Drawing.Point(130, 100),
+                Size = new System.Drawing.Size(80, 25)
+            };
+            cancelButton.Click += (sender, e) =>
+            {
+                DialogResult = DialogResult.Cancel;
+                Close();
+            };
+            this.Controls.Add(activityTypeLabel);
+            this.Controls.Add(activityTypeTextBox);
+            this.Controls.Add(durationLabel);
+            this.Controls.Add(durationTextBox);
+            this.Controls.Add(okButton);
+            this.Controls.Add(cancelButton);
+        }
+    }
+
+}
