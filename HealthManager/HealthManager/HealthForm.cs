001942e6aaff1e82c20939e82bef27a6f7a1b906
semkaakr <christinasemenyuk888@gmail.com>
2026-03-17 23:55:34 +0300

    3. Создан графический интерфейс с элементами управравления HealthForm.cs

---
+++ HealthManager/HealthManager/HealthForm.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HealthManager
+{
+    public partial class HealthForm : Form
+    {
+        private HealthManager healthManager;
+        private Button trackActivityButton;
+        private Button trackNutritionButton;
+        private Button trackSleepButton;
+        private Button displayReportButton;
+        public HealthForm()
+        {
+            this.Text = "Управление здоровьем";
+            this.Width = 300;
+            this.Height = 200;
+            CreateControls();
+            healthManager = new HealthManager();
+        }
+        private void CreateControls()
+        {
+            trackActivityButton = new Button
+            {
+                Location = new System.Drawing.Point(10, 20),
+                Text = "Отслеживать активность",
+                Size = new System.Drawing.Size(120, 25)
+            };
+            trackActivityButton.Click += (sender, e) =>
+            {
+                var activityForm = new ActivityForm();
+                activityForm.ShowDialog();
+                if (activityForm.DialogResult == DialogResult.OK)
+                {
+                    healthManager.TrackActivity(activityForm.ActivityType, activityForm.Duration);
+                }
+            };
+            trackNutritionButton = new Button
+            {
+                Location = new System.Drawing.Point(140, 20),
+                Text = "Отслеживать питание",
+                Size = new System.Drawing.Size(120, 25)
+            };
+            trackNutritionButton.Click += (sender, e) =>
+            {
+                var nutritionForm = new NutritionForm();
+                nutritionForm.ShowDialog();
+                if (nutritionForm.DialogResult == DialogResult.OK)
+                {
+                    healthManager.TrackNutrition(nutritionForm.FoodItem, nutritionForm.Calories);
+                }
+            };
+            trackSleepButton = new Button
+            {
+                Location = new System.Drawing.Point(10, 50),
+                Text = "Отслеживать сон",
+                Size = new System.Drawing.Size(120, 25)
+            };
+            trackSleepButton.Click += (sender, e) =>
+            {
+                var sleepForm = new SleepForm();
+                sleepForm.ShowDialog();
+                if (sleepForm.DialogResult == DialogResult.OK)
+                {
+                    healthManager.TrackSleep(sleepForm.Date, sleepForm.Hours);
+                }
+            };
+            displayReportButton = new Button
+            {
+                Location = new System.Drawing.Point(140, 50),
+                Text = "Показать отчёт",
+                Size = new System.Drawing.Size(120, 25)
+            };
+            displayReportButton.Click += (sender, e) => healthManager.DisplayActivityReport();
+            this.Controls.Add(trackActivityButton);
+            this.Controls.Add(trackNutritionButton);
+            this.Controls.Add(trackSleepButton);
+            this.Controls.Add(displayReportButton);
+        }
+    }
+
+}
