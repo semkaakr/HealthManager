21a189c4b2600f998f7f052ce07d2321a67a31d8
semkaakr <christinasemenyuk888@gmail.com>
2026-03-17 23:53:28 +0300

    3. Создан графический интерфейс с элементами управравления NutritionForm.cs

---
+++ HealthManager/HealthManager/NutritionForm.cs
@@ -0,0 +1,91 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HealthManager
+{
+    public partial class NutritionForm : Form
+    {
+        private TextBox foodItemTextBox;
+        private TextBox caloriesTextBox;
+        private Label foodItemLabel;
+        private Label caloriesLabel;
+        public string FoodItem { get; private set; }
+        public decimal Calories { get; private set; }
+        public NutritionForm()
+        {
+            this.Text = "Добавить питание";
+            this.Width = 250;
+            this.Height = 150;
+            CreateControls();
+        }
+        private void CreateControls()
+        {
+            foodItemLabel = new Label
+            {
+                Text = "Название пищи:",
+                Location = new System.Drawing.Point(10, 10)
+            };
+            foodItemTextBox = new TextBox
+            {
+                Location = new System.Drawing.Point(10, 30),
+                Size = new System.Drawing.Size(200, 20)
+            };
+            caloriesLabel = new Label
+            {
+                Text = "Калорийность:",
+                Location = new System.Drawing.Point(10, 55)
+            };
+            caloriesTextBox = new TextBox
+            {
+                Location = new System.Drawing.Point(10, 75),
+                Size = new System.Drawing.Size(200, 20)
+            };
+            var okButton = new Button
+            {
+                Text = "OK",
+                Location = new System.Drawing.Point(10, 100),
+                Size = new System.Drawing.Size(80, 25)
+            };
+            okButton.Click += (sender, e) =>
+            {
+                if (decimal.TryParse(caloriesTextBox.Text, out decimal calories))
+                {
+                    FoodItem = foodItemTextBox.Text;
+                    Calories = calories;
+                    DialogResult = DialogResult.OK;
+                    Close();
+                }
+                else
+                {
+                    MessageBox.Show("Пожалуйста, введите корректное значение калорийности.");
+                }
+            };
+            var cancelButton = new Button
+            {
+                Text = "Отмена",
+                Location = new System.Drawing.Point(130, 100),
+                Size = new System.Drawing.Size(80, 25)
+            };
+            cancelButton.Click += (sender, e) =>
+            {
+                DialogResult = DialogResult.Cancel;
+                Close();
+            };
+            this.Controls.Add(foodItemLabel);
+            this.Controls.Add(foodItemTextBox);
+            this.Controls.Add(caloriesLabel);
+            this.Controls.Add(caloriesTextBox);
+            this.Controls.Add(okButton);
+            this.Controls.Add(cancelButton);
+        }
+    }
+
+}
